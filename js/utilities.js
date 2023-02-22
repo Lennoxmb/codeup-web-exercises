f07ffa9f9b2750a12b4e3e7e4fb01ebfd71f6df4
Lennox Ballard <noellemb@gmail.com>
2023-02-22 16:37:48 -0600

    update utilities

--- js/utilities.js
+++ js/utilities.js
@@ -7,3 +7,19 @@ function dollaDollaBills(number, language='en', country="US", currency= 'USD'){
 function isNumberNotNaN(input){
     return !isNaN(parseFloat(input)) && input !== null && typeof input !== 'boolean';
 }
+
+function isNumericOrNumericString(input) {
+    return !isNaN(parseFloat(input)) && input !== null && typeof input !== 'boolean'
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function fastestIsPalindrome(str) {
+    var len = Math.floor(str.length / 2);
+    for (var i = 0; i < len; i++)
+        if (str[i] !== str[str.length - i - 1])
+            return false;
+    return true;
+}
